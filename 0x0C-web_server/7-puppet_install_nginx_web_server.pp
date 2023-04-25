302a43a29a30784b1e50344fe79fcf51b3fd09a1
Hollar14 <i_lawal@outlook.com>
2023-04-25 16:31:56 -0700

    qiuck fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,9 +1,20 @@
-#  Nginx should be listening on port 80
-#  When querying Nginx at its root / with a GET request (requesting a page) using curl, it must return a page that contains the string Hello World!
-#  The redirection must be a “301 Moved Permanently”
-#  Your answer file should be a Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements
+# Install Nginx web server (w/ Puppet)
+package { 'nginx':
+  ensure => installed,
+}
 
-exec { 'server configuration':
-	provider => shell,
-	command	=> 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "server_name_;/a location /redirect_me {\\n\\treturn 301 https:google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart'
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
